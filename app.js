761ed8e50b3e1c03037bf09391eb0679479c66ce
João Vitor <joaovitorti07@gmail.com>
2025-03-17 14:27:52 -0300

    Implementação de tratamento de erros e validação

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let listaAmigos = [];
+let numExecucao = 0;
 const idCampoListaAmigos = 'listaAmigos';
 const idCampoResultado = 'resultado';
 
@@ -7,11 +8,49 @@ const textoAmigoSorteado = `O amigo secreto sorteado é: {nomeAmigo}.`;
 const textoListaAmigosVazia = `Insira nomes de amigos na lista para realizar o sorteio.`;
 const textoIdInexistente = `O campo de id '{id}' informado não existe.`;
 const textoCampoInvalido = `Oops! Insira um {nomeCampo} válido.`;
-const textoParametroObrigatorio = `O parâmetro {parametro} é obrigatório.`;
+const textoErroSorteio = `Erro ao gerar sorteio.`;
+const textoErroParametro = `Um ou mais parâmetros estão ausentes ou inválidos na função '{nomeFuncao}'.`;
 const textoErroInterno = `Erro interno! Contate o administrador do sistema.`;
+
+// Funções para tratamento de erros
+function erroId(id) {
+    if (id) {
+        alert(textoErroInterno);
+        console.error(formatarTextoDinamico(textoIdInexistente, id));
+    } else {
+        erroParametro(erroId.name);
+    }
+}
+
+function erroCampoInvalido(nomeCampo) {
+    if (nomeCampo) {
+        let mensagem = formatarTextoDinamico(textoCampoInvalido, nomeCampo);
+
+        alert(mensagem);
+        console.error(mensagem);
+    } else {
+        erroParametro(erroCampoInvalido.name);
+    }
+}
+
+function erroSorteio() {
+    console.error(textoErroSorteio);
+}
+
+function erroParametro(nomeFuncao) {
+    let nomeMensagem = nomeFuncao ? nomeFuncao : erroParametro.name;
+
+    alert(textoErroInterno);
+    console.error(formatarTextoDinamico(textoErroParametro, nomeMensagem));
+}
 
 // Funções utilitárias
 function sortearItemLista(lista) {
+    if (!lista || !Array.isArray(lista)) {
+        erroParametro(sortearItemLista.name);
+        return false;
+    }
+
     let numMaximoSorteado = lista.length - 1;
     let chaveAleatoria = Math.floor(Math.random() * (numMaximoSorteado + 1));
 
@@ -20,14 +59,18 @@ function sortearItemLista(lista) {
 
 function exibirTextoNaTela(id, texto) {
     let campo = validaElemento(document.getElementById(id), id);
-    validaParam(texto);
+
+    if (!texto) {
+        erroParametro(exibirTextoNaTela.name);
+    }
 
     campo.innerHTML = texto;
 }
 
 function exibirListaNaTela(id, lista) {
-    let elementoListaAmigos = document.getElementById(id);
+    let elementoListaAmigos = validaElemento(document.getElementById(id), id);
     let li = document.createElement('li');
+    
     li.textContent = lista.at(-1);
     elementoListaAmigos.appendChild(li);
 }
@@ -37,6 +80,12 @@ function limparCampo(campo) {
         campo.value = '';
     } else {
         campo.innerHTML = '';
+    }
+}
+
+function limparResultado() {
+    if (numExecucao > 0) {
+        limparCampo(document.getElementById(idCampoResultado));
     }
 }
 
@@ -62,15 +111,11 @@ function campoExiste(campo) {
     return !!campo;
 }
 
-function validaCampoForm(campo, nomeCampo) {
-    if (campoExiste(campo)) {
-        if (campoVazio(campo)) {
-            alert(formatarTextoDinamico(textoCampoInvalido, nomeCampo));
-            return false;
-        }
-    } else {
-        alert(textoErroInterno);
-        console.error(formatarTextoDinamico(textoIdInexistente, campo.id));
+function validaCampoForm(id, nome) {
+    let campo = validaElemento(document.getElementById(id), id);
+
+    if (campo && campoVazio(campo)) {
+        erroCampoInvalido(nome);
         return false;
     }
 
@@ -79,22 +124,11 @@ function validaCampoForm(campo, nomeCampo) {
 
 function validaElemento(elemento, id) {
     if (!campoExiste(elemento)) {
-            alert(textoErroInterno);
-            console.error(formatarTextoDinamico(textoIdInexistente, id));
+            erroId(id);
             return false;
         }
 
     return elemento;
-}
-
-function validaParam(param) {
-    if (!param) {
-        alert(textoErroInterno);
-        console.error(formatarTextoDinamico(textoParametroObrigatorio, param));
-        return false;
-    }
-
-    return param;
 }
 
 // Funções principais
@@ -103,25 +137,35 @@ function setListaAmigos(valor) {
 }
 
 function adicionarAmigo() {
+    let campoAmigo = validaCampoForm('amigo', 'nome');
 
     if (campoAmigo) {
         listaAmigos.push(formatarNome(campoAmigo.value));
 
-    limparCampo(campoAmigo);
-    exibirListaNaTela(idCampoListaAmigos, listaAmigos);
+        if (listaAmigos.length == 1) {
+            limparResultado();
+        }
+
+        limparCampo(campoAmigo);
+        exibirListaNaTela(idCampoListaAmigos, listaAmigos);
     }
 }
 
 function sortearAmigo() {
     if (listaAmigos.length > 0) {
         let amigoSorteado = sortearItemLista(listaAmigos);
-        let campoListaAmigos = document.getElementById(idCampoListaAmigos);
+        let campoListaAmigos = validaElemento(document.getElementById(idCampoListaAmigos), idCampoListaAmigos);
+
+        if (!amigoSorteado || !campoListaAmigos) {
+            return erroSorteio();
+        }
 
         exibirTextoNaTela(idCampoResultado, formatarTextoDinamico(textoAmigoSorteado, amigoSorteado));
+        numExecucao++;
         limparCampo(campoListaAmigos);
         setListaAmigos([]);
     } else {
         alert(textoListaAmigosVazia);
-        limparCampo(document.getElementById(idCampoResultado));
+        limparResultado();
     }
 }
